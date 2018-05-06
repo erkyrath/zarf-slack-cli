a9c998367804944a309ade560818934119b25ba8
Andrew Plotkin <zarf@ZarfLent.local>
2018-05-05 20:15:03 -0400

    Try to do thread processing.

--- zlack-client.py
+++ zlack-client.py
@@ -15,7 +15,9 @@ thread = None
 shutdown_thread = False
 
 def check_background(ctx):
-    pass
+    ls = fetch_outputs()
+    for ln in ls:
+        print(ln)
 
 def try_shutdown():
     global shutdown_thread
@@ -27,11 +29,40 @@ def try_shutdown():
 def thread_main():
     global thread
     while not shutdown_thread:
-        print('Tick.')
+        ls = fetch_inputs()
+        for ln in ls:
+            add_output('Processed: ' + ln)
         time.sleep(1)
     print('Disconnected.')
     thread = None
 
+lock = threading.Lock()
+input_list = []
+output_list = []
+
+def add_input(val):
+    with lock:
+        input_list.append(val)
+        
+def fetch_inputs():
+    res = []
+    with lock:
+        if input_list:
+            res.extend(input_list)
+            input_list[:] = []
+    return res
+
+def add_output(val):
+    with lock:
+        output_list.append(val)
+        
+def fetch_outputs():
+    res = []
+    with lock:
+        if output_list:
+            res.extend(output_list)
+            output_list[:] = []
+    return res
 
 thread = threading.Thread(target=thread_main, name='slack-comm')
 thread.start()
@@ -40,17 +71,14 @@ application = prompt_toolkit.shortcuts.create_prompt_application('>')
 
 while thread:
     try:
-        #input = prompt_toolkit.prompt(
-        #    '>',
-        #    eventloop=prompt_toolkit.shortcuts.create_eventloop(inputhook=check_background),
-        #    patch_stdout=True)
-        eventloop = prompt_toolkit.shortcuts.create_eventloop(inputhook=check_background)
         input = prompt_toolkit.shortcuts.run_application(
             application,
             patch_stdout=True,
             refresh_interval=0,
-            eventloop=eventloop)
-        print('### input:', input)
+            eventloop=prompt_toolkit.shortcuts.create_eventloop(inputhook=check_background))
+        input = input.rstrip()
+        if input:
+            add_input(input)
     except KeyboardInterrupt:
         res = try_shutdown()
         if res:

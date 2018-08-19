1a2e6b7262accd364c738300db56884deba6ad3b
Andrew Plotkin <zarf@ZarfLent.local>
2018-08-19 11:45:41 -0400

    Don't repeat sent messages.

--- zlackcli/ui.py
+++ zlackcli/ui.py
@@ -103,12 +103,7 @@ class UI:
             if not origmsg:
                 self.print('Mismatched reply_to (id %d, msg %s)' % (msg.get('reply_to'), msg.get('text')))
                 return
-            chanid = origmsg.get('channel', '')
-            userid = origmsg.get('user', '')
-            # Print our successful messages even on muted channels
-            text = self.decode_message(team, msg.get('text'), msg.get('attachments'))
-            val = '[%s/%s] %s: %s' % (self.team_name(team), self.channel_name(team, chanid), self.user_name(team, userid), text)
-            self.print(val)
+            # We should already have received (and printed) the sent message.
             return
         
         if typ == 'hello':

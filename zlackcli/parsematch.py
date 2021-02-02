e209e39853d7fce89a5e4c4f3b0ce811d2320e14
Andrew Plotkin <zarf@ZarfLent.local>
2021-02-01 19:58:29 -0500

    Comments.

--- zlackcli/parsematch.py
+++ zlackcli/parsematch.py
@@ -1,6 +1,11 @@
 from enum import IntEnum
 
 class ParseMatch:
+    """When parsing user commands, we have to do a lot of partial matching
+    (against team and channel names). The ParseMatch class wraps this up
+    in a handy interface.
+    """
+    
     class Res(IntEnum):
         NONE = 0
         APPROX = 1
@@ -8,10 +13,17 @@ class ParseMatch:
 
     @staticmethod
     def nevermatch(text):
+        """A ParseMatch which never matches. This is meant to be used
+        as a default value.
+        """
         return ParseMatch.Res.NONE
     
     @staticmethod
     def list_best(ls):
+        """Given a list of (result, value) pairs, return the first value
+        whose result is EXACT. If there are none, return the first value
+        whose value is APPROX.
+        """
         Res = ParseMatch.Res
 
         best = None
@@ -24,6 +36,13 @@ class ParseMatch:
         
     @staticmethod
     def match_list(matchers, ls):
+        """Given a list of ParseMatch objects and an (equal) list of
+        strings, check them elementwise. Return the worst result.
+
+        (That is, if all results are EXACT, this returns EXACT. If they're
+        a mix of EXACT and APPROX, this returns APPROX. If any are NONE,
+        this returns NONE.)
+        """
         Res = ParseMatch.Res
 
         if not matchers:
@@ -41,6 +60,9 @@ class ParseMatch:
         return final
         
     def __init__(self, id, aliases=None):
+        """id is the name to match against; aliases is an optional collection
+        of aliases.
+        """
         self.id = id.lower()
         self.aliases = None
         if aliases:
@@ -54,6 +76,14 @@ class ParseMatch:
         return '<ParseMatch "%s">' % (val,)
 
     def __call__(self, text):
+        """self(text) checks whether the text matches the name or any of
+        the aliases. An exact match returns Res.EXACT. If the text is
+        a (non-empty) prefix, it returns Res.APPROX. Otherwise it returns
+        Res.NONE.
+
+        The text argument *must* be lowercased. (We don't do this because
+        it would be terrible inefficient. So it's the caller's job.)
+        """
         Res = ParseMatch.Res
         
         if self.id == text:
